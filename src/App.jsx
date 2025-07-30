d8ef93231bfb1d61d4abf0659e1ec980684fdda8
suhas152 <vedasuhaskulkurni@gmail.com>
2025-07-30 10:43:48 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className='container'>
       <h1> Welcome to my react project-S105</h1>
-      <p>This is a simple home page deployed on Git Hub Pages Using Vite.</p>
+      <h1>CI/CD and Cloud DevOps Course</h1>
+<p>This is a simple home page deployed on Git Hub Pages Using Vite.</p>
 
     </div>
   )
