4be9d3fa0fd3b85dd62698dc7cd2369b6bfaeb22
suhas152 <vedasuhaskulkurni@gmail.com>
2025-07-30 10:17:15 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './style.css'
+export default function App() {
+  return (
+    <div className='container'>
+      <h1> Welcome to my react project-S105</h1>
+      <p>This is a simple home page deployed on Git Hub Pages Using Vite.</p>
+
+    </div>
+  )
+}
